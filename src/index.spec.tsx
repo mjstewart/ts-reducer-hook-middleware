cdfc2a4a81c7f8f8a11a899d8dd9eec1214eff95
Matt Stewart <smtp.matt@gmail.com>
2019-03-31 21:42:25 +1100

    added tests

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -6,117 +6,47 @@ import * as renderer from 'react-test-renderer';
 const voidDispatch = () => {};
 
 interface TestState {
-  count: number;
-  history: string[];
+  logs: string[];
 }
 
- type ActionPayload = { value: number, message: string }
+type TestAction = { type: 'log'; log: string };
 
-type TestAction =
-  | { type: 'inc'; payload: ActionPayload }
-  | { type: 'dec'; payload: ActionPayload }
-  | { type: 'set'; payload: ActionPayload };
+const logAction: (log: string) => TestAction = (log: string) => ({
+  type: 'log',
+  log
+});
 
 const testReducer: Reducer<TestState, TestAction> = (
   state: TestState,
   action: TestAction
 ) => {
-  console.log(`testReducer ${JSON.stringify(action.payload)}`);
   switch (action.type) {
-    case 'inc':
+    case 'log':
       return {
         ...state,
-        count: state.count + action.payload.value,
-        history: [...state.history, action.payload.message]
+        logs: [...state.logs, action.log]
       };
-    case 'dec':
-      return {
-        ...state,
-        count: state.count - action.payload.value,
-        history: [...state.history, action.payload.message]
-      };
-    case 'set':
-      return {
-        ...state,
-        count: action.payload.value,
-        history: [...state.history, action.payload.message]
-      };
-    default:
-      const exhaustiveCheck: never = action;
-      throw new Error(`unhandled cases detected ${exhaustiveCheck}`);
   }
 };
 
-test('two plus four is 6', () => {
-  expect(2 + 2).toBe(4);
-});
-
-// const middlewareOne: Middleware<
-//   TestState,
-//   TestAction
-// > = api => next => action => {
-//   console.log('in middlewareOne before dispatch');
-//   api.dispatch({
-//     type: 'set',
-//     payload: { value: 17, message: 'middlewareOne' }
-//   });
-//   next(action);
-//   console.log('in middlewareOne after next');
-// };
-
-// const middlewareTwo: Middleware<
-//   TestState,
-//   TestAction
-// > = api => next => action => {
-//   console.log('in middlewareTwo before dispatch');
-//   api.dispatch({
-//     type: 'set',
-//     payload: { value: 23, message: 'middlewareTwo' }
-//   });
-//   next(action);
-//   console.log('in middlewareTwo after next');
-// };
-
-// const middlewareThree: Middleware<
-//   TestState,
-//   TestAction
-// > = api => next => action => {
-//   console.log('in middlewareThree before dispatch');
-//   api.dispatch({
-//     type: 'set',
-//     payload: { value: 134, message: 'middlewareThree' }
-//   });
-//   next(action);
-//   console.log('in middlewareThree after next');
-// };
-
 test('middlewares called from left to right when the next dispatch function is called last', () => {
-  console.log('middlewares called from left to right when the next dispatch function is called last')
+  // given middlewares [a, b, c], calling the next dispatch last is the same as a loop which runs in sequence.
 
   let dispatch: React.Dispatch<TestAction> = voidDispatch;
   const TestCounter: React.SFC = () => {
     const { store, dispatch: enhancedDispatch } = useReducerWithMiddleware<
       typeof testReducer
-    >(testReducer, { count: 0, history: [] })([
-      api => next => action => { 
-        api.dispatch({
-          type: 'inc',
-          payload: { value: 50, message: 'middlewareOne' }
-        });
+    >(testReducer, { logs: [] })([
+      api => next => action => {
+        api.dispatch(logAction('middlewareOne'));
         next(action);
       },
-      api => next => action => { 
-        api.dispatch({
-          type: 'dec',
-          payload: { value: 5, message: 'middlewareTwo' }
-        });
+      api => next => action => {
+        api.dispatch(logAction('middlewareTwo'));
         next(action);
       },
-      api => next => action => { 
-        api.dispatch({
-          type: 'inc',
-          payload: { value: 100, message: 'middlewareThree' }
-        });
+      api => next => action => {
+        api.dispatch(logAction('middlewareThree'));
         next(action);
       }
     ]);
@@ -124,10 +54,9 @@ test('middlewares called from left to right when the next dispatch function is c
     dispatch = enhancedDispatch;
     return (
       <div>
-        <h1>{store.count}</h1>
         <ul>
-          {store.history.map(message => (
-            <li key={message}>{message}</li>
+          {store.logs.map(log => (
+            <li key={log}>{log}</li>
           ))}
         </ul>
       </div>
@@ -138,74 +67,50 @@ test('middlewares called from left to right when the next dispatch function is c
   const testInstance = testRenderer.root;
 
   renderer.act(() => {
-    dispatch({ type: 'inc', payload: { value: 1, message: 'initial' } });
+    dispatch(logAction('initial'));
   });
 
-  expect(testInstance.findAllByType('h1')[0].children[0]).toBe('146');
- 
   expect(testInstance.findAllByType('li')[0].children[0]).toBe('middlewareOne');
   expect(testInstance.findAllByType('li')[1].children[0]).toBe('middlewareTwo');
-  expect(testInstance.findAllByType('li')[2].children[0]).toBe('middlewareThree');
+  expect(testInstance.findAllByType('li')[2].children[0]).toBe(
+    'middlewareThree'
+  );
+
+  // last call to the next dispatcher in middlewareThree ends up running the original action supplied to the composed dispatcher.
   expect(testInstance.findAllByType('li')[3].children[0]).toBe('initial');
-
-
-  console.log(testInstance.findAllByType('li')[0].children[0]);
-  console.log(testInstance.findAllByType('li')[1].children[0]);
-  console.log(testInstance.findAllByType('li')[2].children[0]);
-  console.log(testInstance.findAllByType('li')[3].children[0]);
 });
 
-
-
-
-
-
-
-
-
-
-
 test('middlewares called from right to left when the next dispatch function is called first', () => {
-  console.log('middlewares called from right to left when the next dispatch function is called first')
-
+  // given middlewares [a, b, c], calling the 'next' dispatch first is the same as recursing as deep as possible until 'c'.
+  // 'c' is essentially the base case triggering the original action passed into the composed dispatcher. 
+  // Finally, unwind back to 'a' which calls the original dispatch function that hits the reducer.
+  
   let dispatch: React.Dispatch<TestAction> = voidDispatch;
   const TestCounter: React.SFC = () => {
     const { store, dispatch: enhancedDispatch } = useReducerWithMiddleware<
       typeof testReducer
-    >(testReducer, { count: 0, history: [] })([
-      api => next => action => { 
+    >(testReducer, { logs: [] })([
+      api => next => action => {
         next(action);
-        api.dispatch({
-          type: 'inc',
-          payload: { value: 50, message: 'middlewareOne' }
-        });
- 
+        api.dispatch(logAction('middlewareOne'));
       },
-      api => next => action => { 
+      api => next => action => {
         next(action);
-        api.dispatch({
-          type: 'dec',
-          payload: { value: 5, message: 'middlewareTwo' }
-        });
- 
+        api.dispatch(logAction('middlewareTwo'));
       },
-      api => next => action => { 
+      api => next => action => {
         next(action);
-        api.dispatch({
-          type: 'inc',
-          payload: { value: 100, message: 'middlewareThree' }
-        });
-  
+        api.dispatch(logAction('middlewareThree'));
       }
     ]);
 
     dispatch = enhancedDispatch;
+    
     return (
       <div>
-        <h1>{store.count}</h1>
         <ul>
-          {store.history.map(message => (
-            <li key={message}>{message}</li>
+          {store.logs.map(log => (
+            <li key={log}>{log}</li>
           ))}
         </ul>
       </div>
@@ -216,19 +121,80 @@ test('middlewares called from right to left when the next dispatch function is c
   const testInstance = testRenderer.root;
 
   renderer.act(() => {
-    dispatch({ type: 'inc', payload: { value: 1, message: 'initial' } });
+    dispatch(logAction('initial'));
   });
 
-  expect(testInstance.findAllByType('h1')[0].children[0]).toBe('146');
- 
   expect(testInstance.findAllByType('li')[0].children[0]).toBe('initial');
-  expect(testInstance.findAllByType('li')[1].children[0]).toBe('middlewareThree');
+  expect(testInstance.findAllByType('li')[1].children[0]).toBe(
+    'middlewareThree'
+  );
   expect(testInstance.findAllByType('li')[2].children[0]).toBe('middlewareTwo');
   expect(testInstance.findAllByType('li')[3].children[0]).toBe('middlewareOne');
+});
 
+test('middlewares can read initial store', () => {
+  const initialState: TestState = { logs: ['initialState'] };
+  let store: TestState = initialState;
+  let dispatch: React.Dispatch<TestAction> = voidDispatch;
+  
+  const TestCounter: React.SFC = () => {
+    const {
+      store: initialStore,
+      dispatch: enhancedDispatch
+    } = useReducerWithMiddleware<typeof testReducer>(testReducer, initialState)(
+      [
+        api => next => action => {
+          api.dispatch(logAction(`middlewareOne, ${api.getState().logs}`));
+          next(action);
+        },
+        api => next => action => {
+          api.dispatch(logAction(`middlewareTwo, ${api.getState().logs}`));
+          next(action);
+        },
+        api => next => action => {
+          api.dispatch(logAction(`middlewareThree, ${api.getState().logs}`));
+          next(action);
+        }
+      ]
+    );
 
-  console.log(testInstance.findAllByType('li')[0].children[0]);
-  console.log(testInstance.findAllByType('li')[1].children[0]);
-  console.log(testInstance.findAllByType('li')[2].children[0]);
-  console.log(testInstance.findAllByType('li')[3].children[0]);
+    dispatch = enhancedDispatch;
+    store = initialStore;
+
+    return (
+      <div>
+        <ul>
+          {store.logs.map(log => (
+            <li key={log}>{log}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const testRenderer = renderer.create(<TestCounter />);
+  const testInstance = testRenderer.root;
+
+  renderer.act(() => {
+    dispatch(logAction(`first, ${initialState.logs}`));
+  });
+
+  // initial store state
+  expect(testInstance.findAllByType('li')[0].children[0]).toBe('initialState');
+
+  // dispatch effects
+  expect(testInstance.findAllByType('li')[1].children[0]).toBe(
+    'middlewareOne, initialState'
+  );
+  expect(testInstance.findAllByType('li')[2].children[0]).toBe(
+    'middlewareTwo, initialState'
+  );
+  expect(testInstance.findAllByType('li')[3].children[0]).toBe(
+    'middlewareThree, initialState'
+  );
+
+  // last call to the next dispatcher in middlewareThree ends up running the original action supplied to the composed dispatcher.
+  expect(testInstance.findAllByType('li')[4].children[0]).toBe(
+    'first, initialState'
+  );
 });
